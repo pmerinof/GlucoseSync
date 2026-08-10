031fbfbdfbb52111a4c0d89b8f61591beb5fb557
pmerinof <pmerinof@hotmail.com>
2026-08-10 14:15:54 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
 import os
+from pathlib import Path
 
-# Leer credenciales de Abbott desde variables de entorno
+BASE_DIR = Path(__file__).resolve().parent
+
 EMAIL = os.getenv("ABBOTT_EMAIL")
 PASSWORD = os.getenv("ABBOTT_PASSWORD")
 
 if not EMAIL or not PASSWORD:
     raise RuntimeError(
-        "No se han configurado las variables de entorno ABBOTT_EMAIL y ABBOTT_PASSWORD"
+        "No se han configurado las variables de entorno "
+        "ABBOTT_EMAIL y ABBOTT_PASSWORD."
     )
+
+DB_PATH = BASE_DIR / "database" / "glucosesync.db"
+CSV_PATH = BASE_DIR / "datos" / "glucose_history.csv"
+EXCEL_FILE = BASE_DIR / "datos" / "Glucosa.xlsx"
